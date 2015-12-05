8bed21b64d9a968bc8234065e1e03aceec2fb048
Ashansa Perera <ashansa.perera@gmail.com>
2015-12-06 00:39:17 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,7 +11,7 @@ default[:flink][:hadoop_version]                   = "27"
 default[:flink][:scala_version]                   = "2.11"
 default[:flink][:dir]                              = "/usr/local"
 default[:flink][:home]                             = "#{node[:flink][:dir]}/flink-#{node[:flink][:version]}"
-default[:flink][:url]                              = "https://www.apache.org/dist/flink/flink-#{node[:flink][:version]}/flink-#{node[:flink][:version]}-bin-hadoop#{node[:flink][:hadoop_version]}.tgz"
+default[:flink][:url]                              = "http://archive.apache.org/dist/flink/flink-#{node[:flink][:version]}/flink-#{node[:flink][:version]}-bin-hadoop#{node[:flink][:hadoop_version]}.tgz"
 default[:flink][:conf_dir]                         = "#{node[:flink][:home]}/conf"
 
 default[:flink][:checksum]                         = ""
